c8a6d6a98763ba74e76de3936fd87fd9693c5384
David Precious <davidp@preshweb.co.uk>
2009-01-05 17:10:49 +0000

    Preparing version 0.04 of Finance::PremiumBonds.  Simple change - don't output HTML to STDERR (a left-over debugging aid).
    This has been like this for a long time.  Presumably, not a single soul
    uses this module besides me.  Meh :)
    
    
    git-svn-id: file:///shared/svn/scripts/projects/perl/modules/Finance-PremiumBonds@491 c1e6fe2f-0919-0410-96f0-e3221d39872c

--- lib/Finance/PremiumBonds.pm
+++ lib/Finance/PremiumBonds.pm
@@ -8,7 +8,7 @@ use warnings;
 use WWW::Mechanize;
 use Carp;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 our $checker_url  = 'http://www.nsandi.com/products/pb/haveYouWon.jsp';
 our $agent_string = "Perl/Finance::PremiumBonds $VERSION";
 our $holdernumfield = 'holderNumber';
@@ -40,8 +40,6 @@ sub has_won {
     #$mech->field('check', 'go');
     $mech->submit()
         or  warn "Unable to submit lookup - " . $mech->response->status_line 
-        and return;
-    warn "Content: " . $mech->content . "\n";
     if ($mech->content =~ /holder number must be 10 numbers/msi
      || $mech->content =~ /check your holder's number - it is not valid/msi) 
     {
